00b89706ed27a22b8cd5df9d55f7dc06e5052bc4
mond <revive29@gmail.com>
2022-04-14 20:26:37 +0900

    test(BaseballTest): BeforeEach 추가
    - 중복된 코드(Baseball.start())를 BeforeEach로 공통화

--- src/test/java/baseball/model/BaseballTest.java
+++ src/test/java/baseball/model/BaseballTest.java
@@ -6,24 +6,27 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import baseball.model.baseball.Baseball;
 import baseball.model.baseball.constants.BaseballMessage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BaseballTest {
+    private Baseball baseball;
+
+    @BeforeEach
+    void 야구게임_시작() {
+        baseball = Baseball.start();
+    }
 
     @Test
-    void 야구게임_시작() {
-        Baseball baseball = Baseball.start();
-
+    void 야구게임이_생성되어_있다() {
         assertThat(baseball).isNotNull();
     }
 
     @ParameterizedTest(name = "숫자포맷이 맞지 않는 값({0})은 에러 발생")
     @ValueSource(strings = {"5197", " 232", "테스트", "902", "", "test", "-251", "+193"})
     void 숫자포맷이_올바르지_않는_경우(String invalidNumber) {
-        Baseball baseball = Baseball.start();
-
         assertThatThrownBy(() -> baseball.validate(invalidNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(BaseballMessage.INVALID_NUMBER_FORMAT);
@@ -31,7 +34,6 @@ class BaseballTest {
 
     @Test
     void 다른_자릿수에_중복된_수가_있는_경우() {
-        Baseball baseball = Baseball.start();
         String duplicateNumber = "155";
 
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -41,7 +43,6 @@ class BaseballTest {
 
     @Test
     void 입력받은_수를_계산_후_결과반환() {
-        Baseball baseball = Baseball.start();
         String inputNumber = "152";
         baseball.count(inputNumber);
 
