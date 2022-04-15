7a5ffdd5a425e07d40a5b44c0461c053d8c0f5f8
mond <revive29@gmail.com>
2022-04-16 00:05:07 +0900

    test(BaseballCounterTest, BaseballNumberTest):  야구게임 결과를 예상값으로 검증
    - 결과값 제공으로 추상적(숫자의 길이, 양수여부)에서 명시적(제공된 결과값) 비교하도록 BaseballCounterTest 수정
    - 반복 횟수만큼 테스트 진행하도록 BaseballNumberTest 수정

--- src/test/java/baseball/model/BaseballCounterTest.java
+++ src/test/java/baseball/model/BaseballCounterTest.java
@@ -21,14 +21,14 @@ class BaseballCounterTest {
     }
 
     @ParameterizedTest(name = "컴퓨터의 숫자({0})과 유저의 숫자({1}) 모두 스트라이크")
-    @CsvSource(value = {"152:152", "9184:9184", "491:491"}, delimiter = ':')
-    void 결과가_모두_스트라이크만_있는_경우(String computerNumber, String userNumber) {
+    @CsvSource(value = {"152:152:3", "9184:9184:4", "491:491:3"}, delimiter = ':')
+    void 결과가_모두_스트라이크만_있는_경우(String computerNumber, String userNumber, int strikeCount) {
         BaseballCounter counter = BaseballCounter.create(computerNumber);
         counter.count(userNumber);
 
         assertAll(
                 () -> 두_배열_스트라이크_만장일치(computerNumber.toCharArray(), userNumber.toCharArray()),
-                () -> assertThat(counter.getStrike()).isEqualTo(computerNumber.length()),
+                () -> assertThat(counter.getStrike()).isEqualTo(strikeCount),
                 () -> assertThat(counter.getBall()).isZero(),
                 () -> assertThat(counter.isStrike()).isTrue(),
                 () -> assertThat(counter.isBall()).isFalse()
@@ -40,14 +40,14 @@ class BaseballCounterTest {
     }
 
     @ParameterizedTest(name = "컴퓨터의 숫자({0})과 유저의 숫자({1}) 모두 볼")
-    @CsvSource(value = {"152:215", "9184:1849", "491:149"}, delimiter = ':')
-    void 결과가_모두_볼만_있는_경우(String computerNumber, String userNumber) {
+    @CsvSource(value = {"152:215:3", "9184:1849:4", "491:149:3"}, delimiter = ':')
+    void 결과가_모두_볼만_있는_경우(String computerNumber, String userNumber, int ballCount) {
         BaseballCounter counter = BaseballCounter.create(computerNumber);
         counter.count(userNumber);
 
         assertAll(
                 () -> 두_배열_볼_만장일치(computerNumber.toCharArray(), userNumber.toCharArray()),
-                () -> assertThat(counter.getBall()).isEqualTo(computerNumber.length()),
+                () -> assertThat(counter.getBall()).isEqualTo(ballCount),
                 () -> assertThat(counter.getStrike()).isZero(),
                 () -> assertThat(counter.isStrike()).isFalse(),
                 () -> assertThat(counter.isBall()).isTrue()
@@ -66,13 +66,13 @@ class BaseballCounterTest {
     }
 
     @ParameterizedTest(name = "컴퓨터의 숫자({0})과 유저의 숫자({1}) 일부만 스트라이크")
-    @CsvSource(value = {"152:196", "9184:6127", "491:498"}, delimiter = ':')
-    void 결과가_스트라이크가_일부만_있는_경우(String computerNumber, String userNumber) {
+    @CsvSource(value = {"152:196:1", "9184:6127:1", "491:498:2"}, delimiter = ':')
+    void 결과가_스트라이크가_일부만_있는_경우(String computerNumber, String userNumber, int strikeCount) {
         BaseballCounter counter = BaseballCounter.create(computerNumber);
         counter.count(userNumber);
 
         assertAll(
-                () -> assertThat(counter.getStrike()).isPositive(),
+                () -> assertThat(counter.getStrike()).isEqualTo(strikeCount),
                 () -> assertThat(counter.getBall()).isZero(),
                 () -> assertThat(counter.isStrike()).isTrue(),
                 () -> assertThat(counter.isBall()).isFalse()
@@ -80,13 +80,13 @@ class BaseballCounterTest {
     }
 
     @ParameterizedTest(name = "컴퓨터의 숫자({0})과 유저의 숫자({1}) 일부만 볼")
-    @CsvSource(value = {"152:217", "9184:1867", "491:156"}, delimiter = ':')
-    void 결과가_볼이_일부만_있는_경우(String computerNumber, String userNumber) {
+    @CsvSource(value = {"152:217:2", "9184:1867:2", "491:156:1"}, delimiter = ':')
+    void 결과가_볼이_일부만_있는_경우(String computerNumber, String userNumber, int ballCount) {
         BaseballCounter counter = BaseballCounter.create(computerNumber);
         counter.count(userNumber);
 
         assertAll(
-                () -> assertThat(counter.getBall()).isPositive(),
+                () -> assertThat(counter.getBall()).isEqualTo(ballCount),
                 () -> assertThat(counter.getStrike()).isZero(),
                 () -> assertThat(counter.isStrike()).isFalse(),
                 () -> assertThat(counter.isBall()).isTrue()
@@ -94,14 +94,14 @@ class BaseballCounterTest {
     }
 
     @ParameterizedTest(name = "컴퓨터의 숫자({0})과 유저의 숫자({1}) 스트라이크, 볼")
-    @CsvSource(value = {"152:125", "9184:1782", "491:418"}, delimiter = ':')
-    void 결과가_스트라이크_볼_함께_있는_경우(String computerNumber, String userNumber) {
+    @CsvSource(value = {"152:125:1:2", "9184:1784:2:1", "491:418:1:1"}, delimiter = ':')
+    void 결과가_스트라이크_볼_함께_있는_경우(String computerNumber, String userNumber, int strikeCount, int ballCount) {
         BaseballCounter counter = BaseballCounter.create(computerNumber);
         counter.count(userNumber);
 
         assertAll(
-                () -> assertThat(counter.getStrike()).isPositive(),
-                () -> assertThat(counter.getBall()).isPositive(),
+                () -> assertThat(counter.getStrike()).isEqualTo(strikeCount),
+                () -> assertThat(counter.getBall()).isEqualTo(ballCount),
                 () -> assertThat(counter.isStrike()).isTrue(),
                 () -> assertThat(counter.isBall()).isTrue()
         );
--- src/test/java/baseball/model/BaseballNumberTest.java
+++ src/test/java/baseball/model/BaseballNumberTest.java
@@ -3,13 +3,15 @@ package baseball.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import baseball.model.baseball.BaseballNumber;
 import baseball.constants.baseball.BaseballOption;
+import baseball.model.baseball.BaseballNumber;
 import java.util.HashSet;
 import java.util.Set;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class BaseballNumberTest {
+    private final static int REPEAT_NUMBER = 100;
 
     @Test
     void 숫자를_생성() {
@@ -21,21 +23,21 @@ class BaseballNumberTest {
         );
     }
 
-    @Test
+    @RepeatedTest(value = REPEAT_NUMBER, name = "숫자 여부 확인 {currentRepetition} / {totalRepetitions}")
     void 숫자_여부_확인() {
         BaseballNumber number = BaseballNumber.create();
 
         assertThat(number.getNumber()).containsOnlyDigits();
     }
 
-    @Test
+    @RepeatedTest(value = REPEAT_NUMBER, name = "자릿수 일치 확인 {currentRepetition} / {totalRepetitions}")
     void 자릿수_일치_확인() {
         BaseballNumber number = BaseballNumber.create();
 
         assertThat(number.getNumber()).containsPattern("\\d{" + BaseballOption.DIGIT_NUMBER_LENGTH + "}");
     }
 
-    @Test
+    @RepeatedTest(value = REPEAT_NUMBER, name = "숫자를 생성 {currentRepetition} / {totalRepetitions}")
     void 숫자가_양수인지_확인() {
         BaseballNumber number = BaseballNumber.create();
         int intNumber = Integer.parseInt(number.getNumber());
@@ -43,7 +45,7 @@ class BaseballNumberTest {
         assertThat(intNumber).isPositive();
     }
 
-    @Test
+    @RepeatedTest(value = REPEAT_NUMBER, name = "중복된 숫자가 없는지 확인 {currentRepetition} / {totalRepetitions}")
     void 중복된_숫자가_없는지_확인() {
         BaseballNumber number = BaseballNumber.create();
         char[] digitNumberArray = number.getNumber().toCharArray();
